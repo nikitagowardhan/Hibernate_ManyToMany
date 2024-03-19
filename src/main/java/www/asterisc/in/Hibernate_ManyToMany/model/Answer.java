40472f7cfaa2689e272a63084502eac77dd52c4c
nikitagowardhan <nikitagowar02@gmail.com>
2024-03-19 15:41:48 +0530

    Hibernate Many To Many using XML in Advance Java

--- src/main/java/www/asterisc/in/Hibernate_ManyToMany/model/Answer.java
+++ src/main/java/www/asterisc/in/Hibernate_ManyToMany/model/Answer.java
@@ -1,5 +1,36 @@
 package www.asterisc.in.Hibernate_ManyToMany.model;
 
+import java.util.List;
+
 public class Answer {
+	
+	private int id;
+	private String answername;    
+	private String postedBy;    
+	private List<Question> questions;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAnswername() {
+		return answername;
+	}
+	public void setAnswername(String answername) {
+		this.answername = answername;
+	}
+	public String getPostedBy() {
+		return postedBy;
+	}
+	public void setPostedBy(String postedBy) {
+		this.postedBy = postedBy;
+	}
+	public List<Question> getQuestions() {
+		return questions;
+	}
+	public void setQuestions(List<Question> questions) {
+		this.questions = questions;
+	}  
 
 }
--- src/main/java/www/asterisc/in/Hibernate_ManyToMany/model/Question.java
+++ src/main/java/www/asterisc/in/Hibernate_ManyToMany/model/Question.java
@@ -1,5 +1,33 @@
 package www.asterisc.in.Hibernate_ManyToMany.model;
 
+import java.util.List;
+
 public class Question {
 
+	private int id;    
+	private String qname;    
+	private List<Answer> answers;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getQname() {
+		return qname;
+	}
+	public void setQname(String qname) {
+		this.qname = qname;
+	}
+	public List<Answer> getAnswers() {
+		return answers;
+	}
+	public void setAnswers(List<Answer> answers) {
+		this.answers = answers;
+	}  
+	
+	
+	
 }
